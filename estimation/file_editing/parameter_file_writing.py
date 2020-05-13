a2ae4650a0b9db0ff946eae47fdfa372a8dee153
kc900201 <nkc900201@gmail.com>
2020-05-13 15:01:36 +0900

    Test run by changing current directory (parameter_file_writing.py) - 05132020

--- estimation/file_editing/parameter_file_writing.py
+++ estimation/file_editing/parameter_file_writing.py
@@ -8,6 +8,7 @@ Created on Tue Sep 14 13:05:58 2020
 Date          Comment
 ========================
 05122020      First revision
+05132020      Test writing file in other directory
 """
 
 import os
@@ -15,22 +16,24 @@ import shutil
 
 if __name__ == '__main__':
     model_arch_comment = '# model parameter\n'
-    model_arch = 'model_arch: MP-C-SPP-FC-LSTM\n'
+    model_arch = 'model_arch: MP-C-RRL-SPP4-LSTM\n'
     input_size = 'input_size: 1000\n'
     hidden_size = 'hidden_size: 100\n'
     roi_bg_comment = '# roi_bg: BG_ZERO | BG_GN std | BG_DP rate - GN:Gaussian Noise, DP:Depression\n'
     roi_bg = 'roi_bg: BG_ZERO\n'
-    comparative_loss_margin = 'comparative_loss_margin: 0.5\n'
+    comparative_loss_margin = 'comparative_loss_margin: 0.3\n'
     risk_type = 'risk_type: seq_risk\n'
     threshold_of_similar_risk = 'threshold_of_similar_risk = 0.1\n'
     optimizer_info_comment_1 = '# adam | adadelta | adagrad [lr=0.001]  |rmsprop [lr=0.01] | momentum_sgd [lr=0.01 momentum=0.9] |\n'
     optimizer_info_comment_2 = '# nesterovag [lr=0.01 momentum=0.9] | rmspropgraves [lr=0.0001 momentum=0.9] | sgd [lr=0.01] | smorms3 [lr=0.001]\n'
-    optimizer_info = 'optimizer_info: adadelta'
+    optimizer_info = 'optimizer_info: adam 0.001 0'
 
-    for i in range(1, 11):
+    for i in range(16, 21):
         # Opening a a specific file, with intention to write
-        paramFile = open('model_ebox_param' + str(i) + '.txt', 'w')
-        model_path = 'model_path: result/yolov3/model/yolov3/backup_ebox_accident_KitDashV_do_20191215/accident_KitDashV_6000_ebox_' + str(i) + '_model.npz\n'
+        os.chdir('E:/AtsumiLabMDS-2/TRIP/Trip2018Q1/Dashcam') # 05132020
+        paramFile = open('model_tbox_param' + str(i) + '.txt', 'w')
+#        model_path = 'model_path: result/yolov3/model/yolov3/backup_ebox_accident_KitDashV_do_20191215/accident_KitDashV_6000_ebox_' + str(i) + '_model.npz\n'
+        model_path = 'model_path: model/yolov3/backup_ebox_accident_A3DKitDashV_rrelu_20200505/accident_KitDashV_6000_ebox_' + str(i - 10) + '_model.npz\n'
     
         # Write text into file
         paramFile.write(model_arch_comment)    
